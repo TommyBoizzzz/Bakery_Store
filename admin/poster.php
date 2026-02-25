0f7c71b1c15903ccad864bcdaa1037604f84d752
TommyBoizzzz <hengchansela168@gmail.com>
2026-02-25 14:21:59 +0700

    11

--- admin/poster.php
+++ admin/poster.php
@@ -1,8 +1,9 @@
 <?php
 include "../config/db.php";   // PDO pgsql connection
 include "Authencation/auth.php";
+ob_start();
 
-$uploadFolder = "admin/assets/images_slide/";
+$uploadFolder = "assets/images_slide/";
 if(!is_dir($uploadFolder)){
     mkdir($uploadFolder,0755,true);
 }
