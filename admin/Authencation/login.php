f62484a6115b458853104cb882222e2dd3b49dce
TommyBoizzzz <hengchansela168@gmail.com>
2026-03-02 16:16:11 +0700

    4.2 Deploy

--- admin/Authencation/login.php
+++ admin/Authencation/login.php
@@ -44,7 +44,7 @@ if (isset($_POST['login'])) {
 <head>
 <meta charset="UTF-8">
 <title>Bakery Admin Login</title>
-<link rel="icon" type="image/png" href="../../assets/images_app/Link2.png">
+<link rel="icon" type="image/png" href="../../assets/images_app/Link.png">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
--- admin/home.php
+++ admin/home.php
@@ -7,7 +7,7 @@ include 'Authencation/auth.php';
 <head>
 <meta charset="UTF-8">
 <title>BaBBoB Bakery ADMIN</title>
-<link rel="icon" type="image/png" href="/assets/images_app/Link2.png">
+<link rel="icon" type="image/png" href="/assets/images_app/Link.png">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
