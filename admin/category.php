2e6979a00b0babe0fc3997f1cbe7b63b403cb109
TommyBoizzzz <hengchansela168@gmail.com>
2026-03-02 16:18:49 +0700

    Go Go

--- admin/category.php
+++ admin/category.php
@@ -50,7 +50,7 @@ $categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Category Management</title>
-<link rel="icon" type="image/png" href="/assets/images_app/Link.png">
+<link rel="icon" type="image/png" href="/assets/images_app/Link2.png">
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap');
